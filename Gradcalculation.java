349f5ff9e46ce3c916f293e215ec987cca9fdfe3
shadabali8307 <shadabali8307@gmail.com>
2025-09-22 20:53:51 +0530

    Add files via upload

---
+++ Gradcalculation.java
@@ -0,0 +1,18 @@
+package com.mycompany.gradcalculation;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+
+/**
+ *
+ * @author HP
+ */
+public class Gradcalculation {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
